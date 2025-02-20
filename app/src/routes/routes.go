cd298e380995ae6fe51b50f9362a52e14d68863a
Everton Mendonca Jesus <evertonmj@gmail.com>
2025-02-20 13:29:47 -0300

    s

--- app/src/routes/routes.go
+++ app/src/routes/routes.go
@@ -8,10 +8,11 @@ import (
 
 func DefineRoutes(e *echo.Echo) {
     e.POST("/products", handlers.AddProduct)
-    e.GET("/products/", handlers.UpdateProductPrices)
+    e.GET("/products/", handlers.GetAllProducts)
     e.GET("/products/:id", handlers.GetProduct)
     e.PUT("/products/:id", handlers.UpdateProductByID)
     e.DELETE("/products/:id", handlers.DeleteProductById)
+
     e.GET("/liveness", handlers.HealthCheck)
     e.GET("/readiness", handlers.Readiness)
 }
